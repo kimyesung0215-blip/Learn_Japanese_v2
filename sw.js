f268d8f8fb79fe66394da6b8b4d476127c198245
yesung <kimyesung0215@gmail.com>
2026-06-11 10:48:20 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'jpapp-v7';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// 앱 셸: 캐시 우선 / CDN(React·Babel·폰트): 네트워크 후 런타임 캐시 → 오프라인 동작 보장
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET') return;
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request, { ignoreSearch: true }).then((hit) =>
+        hit || fetch(e.request).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          return res;
+        })
+      )
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then((hit) =>
+        hit || fetch(e.request).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          return res;
+        }).catch(() => hit)
+      )
+    );
+  }
+});
